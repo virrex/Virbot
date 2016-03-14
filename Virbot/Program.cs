8e9a67e4035c2ebbbe4a45610359378e941ead0b
Viktor Jakobsen <virrex@live.se>
2016-03-14 13:27:09 +0100

    Commit

--- Virbot/Program.cs
+++ Virbot/Program.cs
@@ -9,25 +9,28 @@ namespace Virbot
 {
     class Program
     {
+       public static DiscordClient bot;
         static void Main(string[] args)
         {
-            DiscordClient bot = new DiscordClient();
+            bot = new DiscordClient();
+            
             bot.MessageReceived += Bot_MessageReceived;
             bot.Connect("virbot123@gmail.com", "Kappa123");
+            StartUpInfo();
             bot.Wait();
         }
 
         private static void Bot_MessageReceived(object sender, MessageEventArgs e)
         {
             if (e.Message.IsAuthor) { return; }
-            if(e.Message.Text == "!help")
+            if (e.Message.Text.ToLower().Contains("!help"))
             {
                 e.Message.Channel.SendMessage
                     (
                     e.User.Mention + " Needs backup, let's help! \n !commands : Shows you a list of all commands \n !bdo : Shows you all the commands for Black Desert Online" 
                     );
             }
-            if(e.Message.Text.ToLower() == "!gif")
+            if(e.Message.Text.ToLower().Contains("!gif"))
             {
                 e.Channel.SendFile("images/giphy.gif");
                 Console.WriteLine("Gif sent");
@@ -36,6 +39,21 @@ namespace Virbot
             {
 
             }
+            if (e.Message.Text.ToLower().Contains("!wiki"))
+            {
+                e.Channel.SendMessage(e.User.Mention + " http://www.blackdeserttome.com/wiki/BDO_Tome");
+            }
+        }
+        private static void StartUpInfo()
+        {
+            Console.WriteLine("This is a discord black desert online bot");
+            Console.WriteLine();
+            Console.WriteLine("Currently active in servers: ");
+            foreach (var server in bot.Servers)
+            {
+                Console.WriteLine("- " + server.Name);
+            }
+
         }
     }
 }

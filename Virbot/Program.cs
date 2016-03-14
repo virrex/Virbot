a84723eee735e4381d4f6c1bab930f2bdce5ce97
Viktor Jakobsen <virrex@live.se>
2016-03-14 09:58:08 +0100

    First Commit

---
+++ Virbot/Program.cs
@@ -0,0 +1,30 @@
+﻿using Discord;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Virbot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DiscordClient bot = new DiscordClient();
+            bot.MessageReceived += Bot_MessageReceived;
+            bot.Connect("virbot123@gmail.com", "Kappa123");
+            bot.Wait();
+        }
+
+        private static void Bot_MessageReceived(object sender, MessageEventArgs e)
+        {
+            if (e.Message.IsAuthor) { return; }
+            if(e.Message.Text == "!test")
+            {
+                e.Message.Channel.SendMessage(e.User.Mention + "Testing... Testing... Is this on?");
+            }
+
+        }
+    }
+}
